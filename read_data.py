721fb6f82a30d82b2e24952057f0f1d9b9cd960d
Karol Kocierz <kkna102@gmail.com>
2021-09-11 21:20:22 +0200

    Cleanup and removal of nans

--- read_data.py
+++ read_data.py
@@ -1,15 +1,13 @@
+from numpy import float64
 import pandas as pd
 
 class DataProvider:
     def __init__(self, data_file):
         with open(data_file) as data_file:
-            data = pd.read_csv(data_file, parse_dates=[1], na_values=['ND'])
-            self.data = data[['Time Serie', 'EURO AREA - EURO/US$']]
+            self.data = pd.read_csv(data_file, parse_dates=[1], na_values=['ND']).dropna()
+            self.data = self.data[['Time Serie', 'EURO AREA - EURO/US$']]
             self.data = self.data.rename(columns={'Time Serie': 'date', 'EURO AREA - EURO/US$': 'value'})
+            self.data['value'] = self.data['value'].astype(float64)
 
-    def get_data_sets(self, proportions=(0.7, 0.2, 0.1)):
+    def get_data_sets(self, base_size=4, proportions=(0.7, 0.2, 0.1)):
         pass
-
-
-d = DataProvider("Foreign_Exchange_Rates.csv").data
-print(d['value'].max())
